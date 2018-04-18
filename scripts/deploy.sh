d29d605f84f7d2fe8a959dc8a61c8874e8674740
Chung-Yi Chi <chung-yi@artsymail.com>
2018-04-18 17:56:39 -0400

    Differentiate heroku app name and deploy env in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,9 @@ yarn assets
 gzip -S .cgz $(find public/assets -name '*.css')
 gzip -S .jgz $(find public/assets -name '*.js')
 bucket-assets --bucket artsy-force-$DEPLOY_ENV
-heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=force-$DEPLOY_ENV
+heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=$HEROKU_APP
 if [ -z "$CIRCLE_SHA1" ]; then
-  git push --force git@heroku.com:force-$DEPLOY_ENV.git master
+  git push --force git@heroku.com:$HEROKU_APP.git master
 else
-  git push --force git@heroku.com:force-$DEPLOY_ENV.git $CIRCLE_SHA1:refs/heads/master
+  git push --force git@heroku.com:$HEROKU_APP.git $CIRCLE_SHA1:refs/heads/master
 fi
